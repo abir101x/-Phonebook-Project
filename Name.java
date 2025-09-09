a404498eb2fdf14ff4d43e48f217127d80723622
Abir Chowdhury <chowdhuryabir1564@gmail.com>
2025-09-09 02:06:09 -0400

    Refactor project to full OOP design (v2.0)

---
+++ Name.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Name {
+    String firstName;
+    String lastName;
+
+    void read(Scanner scanner) {
+        lastName = scanner.next();
+        firstName = scanner.next();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Name name = (Name) obj;
+        return firstName.equalsIgnoreCase(name.firstName) && lastName.equalsIgnoreCase(name.lastName);
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
