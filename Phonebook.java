0cdbd01ef433d5ba2fe46522b517079d33e666e9
Abir Chowdhury <chowdhuryabir1564@gmail.com>
2025-09-09 17:18:55 -0400

    Implement Map-based phonebook and CLI args (v3.0)

--- Phonebook.java
+++ Phonebook.java
@@ -1,55 +1,27 @@
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 class Phonebook {
-    private PhonebookEntry[] entries;
-    private int entryCount;
-    public static final int DEFAULT_CAPACITY = 100;
+    private Map<Name, PhonebookEntry> entries;
 
     public Phonebook(String fileName) throws Exception {
-        this.entries = new PhonebookEntry[DEFAULT_CAPACITY];
-        this.entryCount = 0;
+        this.entries = new HashMap<>();
         
         File file = new File(fileName);
         Scanner infile = new Scanner(file);
 
-        while (infile.hasNext() && this.entryCount < DEFAULT_CAPACITY) {
-            this.entries[this.entryCount] = new PhonebookEntry();
-            this.entries[this.entryCount].read(infile);
-            this.entryCount++;
-        }
-
-        if (infile.hasNext()) {
-            infile.close();
-            throw new Exception("*** Exception *** Phonebook capacity exceeded - increase size of underlying array");
+        while (infile.hasNext()) {
+            PhonebookEntry newEntry = new PhonebookEntry();
+            newEntry.read(infile);
+            this.entries.put(newEntry.name, newEntry);
         }
         
         infile.close();
     }
 
-    public int lookup(Name searchName) {
-        for (int i = 0; i < this.entryCount; i++) {
-            if (this.entries[i].name.equals(searchName)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public int reverseLookup(PhoneNumber searchNumber) {
-        for (int i = 0; i < this.entryCount; i++) {
-            if (this.entries[i].phoneNumber.equals(searchNumber)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public PhonebookEntry getEntry(int index) {
-        if (index >= 0 && index < this.entryCount) {
-            return this.entries[index];
-        }
-        return null;
+    public PhonebookEntry lookup(Name searchName) {
+        return this.entries.get(searchName);
     }
 }
