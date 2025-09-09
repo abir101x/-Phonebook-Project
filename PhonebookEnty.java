223f7a15b1142feb5299506a351b70dedabc2f7e
Abir Chowdhury <chowdhuryabir1564@gmail.com>
2025-09-09 17:18:19 -0400

    Update PhonebookEnty.java

--- PhonebookEnty.java
+++ PhonebookEnty.java
@@ -1,21 +1,32 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 class PhonebookEntry {
     Name name;
-    PhoneNumber phoneNumber;
+    ArrayList<PhoneNumber> phoneNumbers;
 
     PhonebookEntry() {
         name = new Name();
-        phoneNumber = new PhoneNumber();
+        phoneNumbers = new ArrayList<>();
     }
 
     void read(Scanner scanner) {
         name.read(scanner);
-        phoneNumber.read(scanner);
+        int count = scanner.nextInt();
+        for (int i = 0; i < count; i++) {
+            PhoneNumber newNumber = new PhoneNumber();
+            newNumber.read(scanner);
+            phoneNumbers.add(newNumber);
+        }
     }
 
     @Override
     public String toString() {
-        return name.toString() + "'s phone number is " + phoneNumber.toString();
+        StringJoiner sj = new StringJoiner(", ");
+        for (PhoneNumber pn : phoneNumbers) {
+            sj.add(pn.toString());
+        }
+        return name.toString() + "'s phone numbers: [" + sj.toString() + "]";
     }
 }
